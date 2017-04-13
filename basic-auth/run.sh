dd913a7ebdfb897f7ff4943edd55ad22e9abe921
Joao Moreno <mail@joaomoreno.com>
2017-04-13 10:58:11 +0200

    add basic-auth

---
+++ basic-auth/run.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+docker build -t squid-basic-auth .
+docker kill squid-basic-auth
+docker rm squid-basic-auth
+docker run --name squid-basic-auth -d --restart=always \
+  --publish 3129:3128 \
+  squid-basic-auth
+
+echo "Running basic auth proxy on port 3129"
+echo "Username: user"
+echo "Password: password"
