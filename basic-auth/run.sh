3799d16700fe3ba9c700fcea0d5f1c9f6405bf64
Joao Moreno <jomo@microsoft.com>
2017-04-18 10:15:59 +0200

    update run.sh

--- basic-auth/run.sh
+++ basic-auth/run.sh
@@ -2,9 +2,7 @@
 docker build -t squid-basic-auth .
 docker kill squid-basic-auth
 docker rm squid-basic-auth
-docker run --name squid-basic-auth -d --restart=always \
-  --publish 3129:3128 \
-  squid-basic-auth
+docker run --name squid-basic-auth -d --restart=always --publish 3129:3128 joaomoreno/squid-basic-auth
 
 echo "Running basic auth proxy on port 3129"
 echo "Username: user"
