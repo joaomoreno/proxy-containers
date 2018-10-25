cb9a894c6cef3f3ef108b3279497f12b3cdc7f54
Joao Moreno <mail@joaomoreno.com>
2018-10-25 12:57:39 -0700

    remove running comment

--- basic/run.sh
+++ basic/run.sh
@@ -3,5 +3,3 @@ docker build -t squid-basic .
 docker kill squid-basic
 docker rm squid-basic
 docker run --name squid-basic -d --restart=always --publish 3128:3128 squid-basic
-
-echo "Running basic proxy on port 3128"
